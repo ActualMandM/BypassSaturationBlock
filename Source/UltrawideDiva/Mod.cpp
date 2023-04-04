015bc7ab8f79bb6d59044ae0613fce9635bab8a2
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-04-03 21:21:06 -0700

    proof of concept 3

--- Source/UltrawideDiva/Mod.cpp
+++ Source/UltrawideDiva/Mod.cpp
@@ -1,10 +1,18 @@
-uint32_t width = 1920;
-uint32_t height = 1080;
+uint32_t width;
+uint32_t height;
 
 void changeAspect()
 {
 	double aspect = (double)width / (double)height;
 
+	WRITE_MEMORY(0x1414ABFE0, uint32_t, 18);
+
+	// idk which one of these correlate to 18
+	WRITE_MEMORY(0x140BF7D94, uint32_t, width, height);
+	WRITE_MEMORY(0x140BF7E28, uint32_t, width, height);
+	WRITE_MEMORY(0x140BF7E3C, uint32_t, width, height);
+
+	/*
 	if (aspect != (16.0 / 9.0))
 	{
 		if (aspect > (16.0 / 9.0))
@@ -22,51 +30,34 @@ void changeAspect()
 			WRITE_MEMORY(0x1417594AC, uint32_t, height);
 		}
 	}
+	*/
 
 	WRITE_MEMORY(0x1414AC020, double, aspect);
 }
 
-// called on res change
-HOOK(void, __fastcall, _1402C27E0, 0x1402C27E0, __int64 a1, int a2, int a3, char a4)
+/*
+	somewhere during pass_sprite (sub_1404DAA00) it gets cropped to 16:9
+*/
+
+HOOK(void, __fastcall, _ResolutionChange, 0x1402C27E0, __int64 a1, int a2, int a3, char a4)
 {
 	printf("[DivaUltrawide] sub_1402C27E0 called: %dx%d\n", a2, a3);
 	width = a2;
 	height = a3;
 
 	WRITE_MEMORY(0x140CCD1C0, double, (double)width / (double)height);
-
-	original_1402C27E0(a1, a2, a3, a4);
+	changeAspect();
+	original_ResolutionChange(a1, a2, a3, a4);
 }
-
-/*
-// called on boot, crashes atm
-HOOK(void, __fastcall, _1402C0B60, 0x1402C0B60)
-{
-	printf("[DivaUltrawide] sub_1402C0B60 called\n");
-	WRITE_MEMORY(0x140CCD1C0, double, 16.0 / 9.0);
-	original_1402C0B60();
-}
-
-// unknown!
-HOOK(void, __fastcall, _14041F200, 0x14041F200)
-{
-	printf("[DivaUltrawide] sub_14041F200 called\n");
-	WRITE_MEMORY(0x140CCD140, float, 16.0f / 9.0f);
-	original_14041F200();
-}
-*/
 
 extern "C"
 {
 	void __declspec(dllexport) Init()
 	{
-		INSTALL_HOOK(_1402C27E0);
-		//INSTALL_HOOK(_1402C0B60);
-		//INSTALL_HOOK(_14041F200);
-	}
+		WRITE_MEMORY(0x140BF7D94, uint32_t, 3440, 1440);
+		WRITE_MEMORY(0x140BF7E28, uint32_t, 3440, 1440);
+		WRITE_MEMORY(0x140BF7E3C, uint32_t, 3440, 1440);
 
-	void __declspec(dllexport) OnResize()
-	{
-		changeAspect();
+		//INSTALL_HOOK(_ResolutionChange);
 	}
 }
